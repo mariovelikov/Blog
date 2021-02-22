79e90c09914ae37e399ff2d8bcbd06d185445b42
mariovelikov <70330129+mariovelikov@users.noreply.github.com>
2021-02-22 19:07:28 +0200

    Show image in landing page !

--- BlogAPP/blogs_system/urls.py
+++ BlogAPP/blogs_system/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('create_blog/', views.CreateBlogView.as_view(), name='create blog'),
@@ -9,4 +11,4 @@ urlpatterns = [
     path('create_post/', views.CreatePost.as_view(), name='create post'),
     # path('<int:pk>/delete_post/', views.DeletePost.as_view(), name='delete post'),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
